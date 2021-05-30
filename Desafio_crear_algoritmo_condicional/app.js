00ef8316a8ddf63c3df551992498ae583321042b
Damian Deza <damiandeza@gmail.com>
2021-05-29 23:45:21 -0300

    Agregado archivos cript aparte y validacion de datos

---
+++ Desafio_crear_algoritmo_condicional/app.js
@@ -0,0 +1,32 @@
+function submit_datos() {
+    nombre = document.querySelector("#nombre").value;
+    apellido = document.querySelector("#apellido").value;
+    edad = document.querySelector("#edad").value;
+
+    if (!DatosValidos(nombre, apellido, edad)) return;
+
+    datos = `Sus datos son:\n\tNombre: ${nombre}\n\tApellido: ${apellido}\n\tEdad: ${edad}`;
+
+    if (edad >= 18) {
+        datos += `\n\tY usted es mayor de edad`;
+    } else {
+        datos += `\n\tY usted es menor de edad`;
+    }
+
+    alert(datos);
+}
+
+function DatosValidos(nombre, apellido, edad) {
+    error = "";
+    if (nombre == "" || apellido == "" || edad == "")
+        error = "Por favor complete los siguientes datos:\n";
+    if (nombre == "") error += "\tNombre\n";
+    if (apellido == "") error += "\tApellido\n";
+    if (edad == "") error += "\tEdad\n";
+
+    if (error != "") {
+        alert(error);
+        return false;
+    }
+    return true;
+}
